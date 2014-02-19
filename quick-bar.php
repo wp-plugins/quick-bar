c5f1dae7e25f9825a8a56b62d054cef5aac4b54f
f1logic <f1logic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-19 08:12:24 +0000

    error reporting updated
    git-svn-id: https://plugins.svn.wordpress.org/quick-bar/tags/1.1@860748 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- quick-bar.php
+++ quick-bar.php
@@ -32,7 +32,7 @@ if ( !function_exists( 'add_action' ) )
 }
 
 ob_start();
-error_reporting(0);
+//error_reporting(0);
 define('XYZ_QBR_PLUGIN_FILE',__FILE__);
 
 require( dirname( __FILE__ ) . '/xyz-functions.php' );
